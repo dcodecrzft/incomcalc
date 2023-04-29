e7778f62e5a20820225e5e0ed78470b029089b64
BS Chetan Swaroop Reddy <cannedocean@protonmail.com>
2023-04-29 10:03:45 +0530

    Initial 2.0

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
-from flask import Flask 
+from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def test():
-    return "Hello World"
+    return "Hello world" #satvik dont change this for a sec #I did not change anything
+        
 
 
 if __name__ == "__main__":
